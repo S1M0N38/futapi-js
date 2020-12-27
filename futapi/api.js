64fa4a67710613f7bd5badaccdc62435110d3fcb
S1M0N38 <bertolottosimone@gmail.com>
2020-12-27 20:05:44 +0100

    Basic search on the market (with tests)

--- futapi/api.js
+++ futapi/api.js
@@ -62,7 +62,8 @@ export default class API {
   async launch () {
     const options = {
       headless: false,
-      userDataDir: this.userDataDir
+      userDataDir: this.userDataDir,
+      slowMo: 50
     }
     this.browser = await puppeteer.launch(options)
     this.page = (await this.browser.pages())[0]
@@ -85,8 +86,10 @@ export default class API {
       await this._login()
     } else { console.log('not need for login.') }
 
-    await this.page.waitForSelector('nav.ut-tab-bar')
-    await this.page.waitForTimeout(2000)
+    await this.page.waitForResponse(
+      'https://utas.external.s3.fut.ea.com/ut/game/fifa21/rivals/user/prizeDetails'
+    )
+    await this.page.waitForSelector('ut-click-shield', { hidden: true })
     this.loggedIn = true
   }
 
@@ -94,18 +97,89 @@ export default class API {
     await this.browser.close()
   }
 
+  async sleep (milliseconds) {
+    milliseconds += milliseconds / 5 * Math.random()
+    await this.page.waitForTimeout(milliseconds)
+  }
+
   async _clickHome () {
+    await this.page.waitForTimeout(1000)
     await this.page.click('button.icon-home')
-    await this.page.waitForSelector('h1.title') // TODO better way to wait
-    await this.sleep(500)
-    console.log('cliked home')
+    await this.page.waitForXPath('//h1[@class = "title" and text() = "Home"]')
+    await this.page.waitForSelector('ut-click-shield', { hidden: true })
+    await this.page.waitForTimeout(1000)
   }
 
   async _clickSquads () {
+    await this.page.waitForTimeout(1000)
     await this.page.click('button.icon-squad')
-    await this.page.waitForSelector('h1.title')
-    await this.sleep(500)
-    console.log('cliked squad')
+    await this.page.waitForXPath('//h1[@class = "title" and text() = "Squads"]')
+    await this.page.waitForSelector('ut-click-shield', { hidden: true })
+    await this.page.waitForTimeout(1000)
+  }
+
+  async _clickSBC () {
+    await this.page.waitForTimeout(1000)
+    await this.page.click('button.icon-sbc')
+    await this.page.waitForXPath('//h1[@class = "title" and text() = "Squad Building Challenges"]')
+    await this.page.waitForSelector('ut-click-shield', { hidden: true })
+    await this.page.waitForTimeout(1000)
+  }
+
+  async _clickTransfers () {
+    await this.page.waitForTimeout(1000)
+    await this.page.click('button.icon-transfer')
+    await this.page.waitForXPath('//h1[@class = "title" and text() = "Transfers"]')
+    await this.page.waitForSelector('ut-click-shield', { hidden: true })
+    await this.page.waitForTimeout(1000)
+  }
+
+  async _clickSearchTheTransferMarket () {
+    await this.page.waitForTimeout(1000)
+    await this.page.click('div.ut-tile-transfer-market')
+    await this.page.waitForXPath('//h1[@class = "title" and text() = "Search the Transfer Market"]')
+    await this.page.waitForSelector('ut-click-shield', { hidden: true })
+    await this.page.waitForTimeout(1000)
+  }
+
+  async _inputSearch (
+    playerName,
+    quality, // 0 => any, 1 => bronze, 2 => silver, 3 => gold, 4 => special
+    rarity,
+    position, chemistryStyle,
+    nationality, league, club,
+    minBidPrice, maxBidPrice,
+    minBuyNowPrice, maxBuyNowPrice) {
+    const filters = await this.page.$$('button.ut-search-filter-control--row-button')
+
+    if (playerName) {
+      await this.page.type('input.ut-text-input-control', playerName)
+      await (await this.page.waitForSelector('ul.playerResultsList > button')).click()
+    }
+
+    // NOT WORKING
+    // if (quality) {
+    //   await filters[0].click()
+    //   const choices = await this.page.$$('ul.inline-list > li')
+    //   await choices[quality].click()
+    // }
+
+    // TODO add others filters
+  }
+
+  async _clickSearch () {
+    // await Promise.all([
+    //   this.page.waitForNavigation({ waitUntil: 'networkidle0' }),
+    //   this.page.click('button.call-to-action'),
+    // ])
+    await this.page.click('button.call-to-action')
+    await this.page.waitForSelector('ut-click-shield', { hidden: true })
+    await this.page.waitForTimeout(5000)
+  }
+
+  async _clickNext () {
+    await this.page.click('button.next')
+    await this.page.waitForTimeout(2000)
   }
 
   async _jsonInterceptor (response) {
@@ -113,11 +187,75 @@ export default class API {
       response.url().match(
         /^https:\/\/utas\.external\.s[1,2,3].fut\.ea\.com\/ut\/game\/fifa21.*/
       ) && response.request().method() === 'GET' && response.ok()
-    ) { console.log(await response.json()) }
+    ) {
+      const url = response.url().split('fifa21')[1].split('?')[0]
+      // console.log(url)
+      switch (url) {
+        // return during webapp launch
+        case '/usermassinfo':
+          this._usermassinfo = await response.json()
+          break
+
+        // returned during webapp launch and sometimes when clicked on transfers
+        case '/tradepile':
+          this._tradepile = await response.json()
+          break
+
+        case '/watchlist':
+          this._watchlist = await response.json()
+          break
+
+        case '/sbs/sets':
+          this._sbcSets = await response.json()
+          break
+
+        case '/transfermarket':
+          this._transfermarket = this._transfermarket.concat(
+            (await response.json()).auctionInfo)
+          break
+
+        default:
+          break
+      }
+    }
   }
 
-  async sleep (milliseconds) {
-    milliseconds += milliseconds / 5 * Math.random()
-    await this.page.waitForTimeout(milliseconds)
+  get usermassinfo () {
+    return new Promise((resolve) => {
+      this._clickHome().then(() => { resolve(this._usermassinfo) })
+    })
+  }
+
+  get tradepile () {
+    return new Promise((resolve) => {
+      this._clickTransfers().then(() => { resolve(this._tradepile) })
+    })
+  }
+
+  get watchlist () {
+    return new Promise((resolve) => {
+      this._clickTransfers().then(() => { resolve(this._watchlist) })
+    })
+  }
+
+  get sbcSets () {
+    return new Promise((resolve) => {
+      this._clickSBC().then(() => { resolve(this._sbcSets) })
+    })
+  }
+
+  async transfermarket (playerName, quality, expiresIn = 0) {
+    this._transfermarket = []
+
+    await this._clickTransfers()
+    await this._clickSearchTheTransferMarket()
+    await this._inputSearch(playerName, quality)
+    await this._clickSearch()
+    await this.page.waitForXPath('//h1[@class = "title" and text() = "Search Results"]')
+    await this.page.waitForSelector('ut-click-shield', { hidden: true })
+    while (this._transfermarket.slice(-1)[0].expires < expiresIn) {
+      await this._clickNext()
+    }
+    console.log(this._transfermarket.length)
   }
 }
--- tests/api.test.js
+++ tests/api.test.js
@@ -1,9 +1,9 @@
 import API from '../futapi/api.js'
 import { LoginError } from '../futapi/errors.js'
-import { user1 } from './users.js'
+import { user1, user2 } from './users.js'
 
-describe('API launch', () => {
-  test.skip('first time login invalid backup codes', async () => {
+describe.skip('API launch', () => {
+  test('first time login invalid backup codes', async () => {
     const fakeCodes = ['73899675', '51840933', '12893787']
     const api = new API(user1.email, user1.password, fakeCodes)
     api.userDataDir = undefined
@@ -12,7 +12,7 @@ describe('API launch', () => {
     await api.close()
   })
 
-  test.skip('first time login', async () => {
+  test('first time login', async () => {
     const api = new API(user1.email, user1.password, user1.codes)
     api.userDataDir = undefined
     await api.launch()
@@ -28,24 +28,66 @@ describe('API launch', () => {
   })
 })
 
-describe.only('API endpoints (no market)', () => {
+describe('API interactions', () => {
   let api
 
   beforeAll(async () => {
-    api = new API(user1.email, user1.password, user1.codes)
+    api = new API(user2.email, user2.password, user2.codes)
     await api.launch()
   })
 
-  test('squads click', async () => {
-    await api._clickSquads()
-    const title = await api.page.$eval('h1.title', el => el.textContent)
-    expect(title).toBe('Squads')
+  describe('API navigation (input & click)', () => {
+    test('squads click', async () => {
+      await api._clickSquads()
+      const title = await api.page.$eval('h1.title', el => el.textContent)
+      expect(title).toBe('Squads')
+    })
+
+    test('home click', async () => {
+      await api._clickHome()
+      const title = await api.page.$eval('h1.title', el => el.textContent)
+      expect(title).toBe('Home')
+    })
+
+    test('SBC click', async () => {
+      await api._clickSBC()
+      const title = await api.page.$eval('h1.title', el => el.textContent)
+      expect(title).toBe('Squad Building Challenges')
+    })
+
+    test('transfers click', async () => {
+      await api._clickTransfers()
+      const title = await api.page.$eval('h1.title', el => el.textContent)
+      expect(title).toBe('Transfers')
+    })
+
+    test('search the transfer market click', async () => {
+      await api._clickSearchTheTransferMarket()
+      const title = await api.page.$eval('h1.title', el => el.textContent)
+      expect(title).toBe('Search the Transfer Market')
+    })
   })
 
-  test('home click', async () => {
-    await api._clickHome()
-    const title = await api.page.$eval('h1.title', el => el.textContent)
-    expect(title).toBe('Home')
+  describe('API endpoints', () => {
+    test('usermassinfo', async () => {
+      expect((await api.usermassinfo).userInfo.credits).toBeDefined()
+    })
+
+    test('tradepile', async () => {
+      expect((await api.tradepile).credits).toBeDefined()
+    })
+
+    test('watchlist', async () => {
+      expect((await api.watchlist).credits).toBeDefined()
+    })
+
+    test('sbcSets', async () => {
+      expect((await api.sbcSets).categories).toBeDefined()
+    })
+
+    test.only('transfermarket', async () => {
+      await api.transfermarket('Theo Hernández', 0, 3000)
+    })
   })
 
   afterAll(async () => {
